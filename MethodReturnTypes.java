75c31e627596bf15e324b2ac53a75b4d798681b9
Jhester Arwen Amboy <amboyjhesterarwen_bscs@plmun.edu.ph>
2025-02-28 01:13:40 +0800

    2025-OBJPROG-LAB014

---
+++ MethodReturnTypes.java
@@ -0,0 +1,19 @@
+public class MethodReturnTypes {
+    public static void main(String[] args) {
+        int value1 = 20;
+        int value2 = 30;
+
+        double result = calculateAverage(value1, value2);
+        public static void displayWelcomeMessage() {
+            System.out.println("Welcome to our program!");
+          }
+          displayWelcomeMessage();
+
+          public static double calculateAverage(int num1, int num2) {
+            double average = (num1 + num2) / 2.0;
+        return average;
+          }
+
+          System.out.println("The average is: " + result);
+    }
+}
